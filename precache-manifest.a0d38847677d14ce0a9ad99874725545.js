3bddbafb364d3db33a92c351df3d82c0fc687045
pabloskyCL <pabloskiquiroz@gmail.com>
2022-01-07 23:22:18 -0300

    Updates

---
+++ precache-manifest.a0d38847677d14ce0a9ad99874725545.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24e891c11697a5eb1b453213082997a",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "fd239602b61a09a42ee6",
+    "url": "/ReactTodoApp/static/css/2.aa530cea.chunk.css"
+  },
+  {
+    "revision": "154f6c3799726600410a",
+    "url": "/ReactTodoApp/static/css/main.0d182f1d.chunk.css"
+  },
+  {
+    "revision": "fd239602b61a09a42ee6",
+    "url": "/ReactTodoApp/static/js/2.b0dfdb4c.chunk.js"
+  },
+  {
+    "revision": "37eb2c1156d93feab9cb5ba5a4caa499",
+    "url": "/ReactTodoApp/static/js/2.b0dfdb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154f6c3799726600410a",
+    "url": "/ReactTodoApp/static/js/main.68ec4706.chunk.js"
+  },
+  {
+    "revision": "6cbb3a5cf6cf8dfabb12",
+    "url": "/ReactTodoApp/static/js/runtime-main.116392a1.js"
+  },
+  {
+    "revision": "155c12f6f51ec6b3e9ebcd75cbe77a8a",
+    "url": "/ReactTodoApp/static/media/MaterialIcons-Regular.155c12f6.eot"
+  },
+  {
+    "revision": "2543e46e76419267db0f5eac2cca78d1",
+    "url": "/ReactTodoApp/static/media/MaterialIcons-Regular.2543e46e.woff2"
+  },
+  {
+    "revision": "2f9c4a1521d5a9efd8bcfe942c568f3a",
+    "url": "/ReactTodoApp/static/media/MaterialIcons-Regular.2f9c4a15.ttf"
+  },
+  {
+    "revision": "e638a36a512bd6e2156d4f6239ac82ac",
+    "url": "/ReactTodoApp/static/media/MaterialIcons-Regular.e638a36a.woff"
+  },
+  {
+    "revision": "0d9d647bf75124146db970afbc897543",
+    "url": "/ReactTodoApp/static/media/fondo_web_principal.0d9d647b.jpeg"
+  }
+]);
